8ecfc2a676fc00f5f0fc5b072eb1c477221a35d7
miroslavmarinov <1028757@ucn.dk>
2023-01-02 14:07:22 +0100

    refactored based on PR, as well as added CI/CD workflow

--- TSOMessageHub/Consumers/SignalConsumer.cs
+++ TSOMessageHub/Consumers/SignalConsumer.cs
@@ -1,22 +1,21 @@
-﻿
-using MassTransit;
+﻿using MassTransit;
 using TSOMessageHub.DTOs;
 
 namespace TSOMessageHub.Consumers;
-	public class SignalConsumer : IConsumer<TSOSignal>
+public class SignalConsumer : IConsumer<TSOSignal>
+{
+    private readonly ILogger<SignalConsumer> _logger;
+
+	public SignalConsumer(ILogger<SignalConsumer> logger)
 	{
-        private readonly ILogger<SignalConsumer> _logger;
+        _logger = logger;
+	}
 
-		public SignalConsumer(ILogger<SignalConsumer> logger)
-		{
-            _logger = logger;
-		}
-
-        public async Task Consume(ConsumeContext<TSOSignal> context)
-        {
-            _logger.LogInformation("Message Sent at: {Time}", context.Message.ReceivedUTC);
-            _logger.LogInformation("Received a new message with id: {Id} and Quantity: {Quantity}", context.Message.SignalId, context.Message.QuantityMw);
-            _logger.LogInformation("Current Time: {Time}", DateTimeOffset.Now.ToString("dd-MM-yyyy HH:mm:ss.fff"));
-        }
+    public async Task Consume(ConsumeContext<TSOSignal> context)
+    {
+        _logger.LogInformation("Message Sent at: {Time}", context.Message.ReceivedUTC);
+        _logger.LogInformation("Received a new message with id: {Id} and Quantity: {Quantity}", context.Message.SignalId, context.Message.QuantityMw);
+        _logger.LogInformation("Current Time: {Time}", DateTimeOffset.Now.ToString("dd-MM-yyyy HH:mm:ss.fff"));
     }
+}
 
