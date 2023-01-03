8f4164675662fd4f00ad86788443dcd6460fba18
miroslavmarinov <1028757@ucn.dk>
2023-01-03 20:36:41 +0100

    Added new Remote Signal service

---
+++ RemoteSignalAPI/Controllers/SignalsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.DTOs;
+
+namespace RemoteSignalAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SignalsController : ControllerBase
+{
+    private readonly ILogger<SignalsController> _logger;
+
+    public SignalsController(ILogger<SignalsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("PostSignal")]
+    public async Task<ActionResult> PostSignal(SignalDTO signalDto)
+    {
+        _logger.LogInformation($"Signal {signalDto.Id} received Remote Signal API");
+        return Ok();
+    }
+}
+
