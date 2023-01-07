438efe9dc76f8c99d41613536f42f013165f0a65
miroslavmarinov <1028757@ucn.dk>
2023-01-07 20:25:29 +0100

    Updated quantity random value generation

--- TSOMessageHub/Jobs/SendXML.cs
+++ TSOMessageHub/Jobs/SendXML.cs
@@ -11,9 +11,12 @@ public class SendXML : IJob
     private readonly char _separator = Path.DirectorySeparatorChar;
     private readonly string _projectPath = new Uri(Directory.GetCurrentDirectory()).LocalPath;
     private readonly static Random _random = new Random();
-    private static decimal _quantityMw = 0;
+    private static decimal _quantityMw = 38;
     private static int _counter = 0;
     private readonly TSOSignal _signal;
+    private static bool _isDown = true;
+    private static int _bottomLimit = -3;
+    private static int _topLimit = 1;
 
     public SendXML(IPublishEndpoint publishEndpoint)
     { 
@@ -30,8 +33,23 @@ public class SendXML : IJob
     public async Task Execute(IJobExecutionContext context)
     {
         _signal.SignalId += ++_counter;
-        _quantityMw += _random.Next(-2, 3);
-        _signal.QuantityMw += _quantityMw;
+        if ((_quantityMw <= -40) && (_isDown))
+        {
+            _quantityMw = -40;
+            _isDown = false;
+            _bottomLimit = 0; 
+            _topLimit = 4;
+        }
+        else if ((_quantityMw >= 40) && (_isDown is false))
+        {
+            _quantityMw = 40;
+            _isDown = true;
+            _bottomLimit = -3;
+            _topLimit = 1;
+        }
+            
+        _quantityMw += _random.Next(_bottomLimit, _topLimit);
+        _signal.QuantityMw = _quantityMw;
         _signal.ReceivedUTC = DateTime.UtcNow.ToString("dd-MM-yyyy HH:mm:ss.fff");
 
         await _publishEndpoint.Publish(_signal);
